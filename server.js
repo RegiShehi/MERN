9e597335e9dbcde62bf46fb3c8a82540cb799db7
Regi Shehi <shehi.regi@gmail.com>
2020-09-05 10:24:42 +0200

    Add user model & api routes

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const users = require('./routes/api/users');
+const posts = require('./routes/api/posts');
+const profile = require('./routes/api/profile');
+const auth = require('./routes/api/auth');
 
 const app = express();
 
@@ -7,6 +11,12 @@ const app = express();
 connectDB();
 
 app.get('/', (req, res, next) => res.send('API running'));
+
+//Define routes
+app.use('/api/users', users);
+app.use('/api/auth', auth);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const PORT = process.env.PORT || 5000;
 
