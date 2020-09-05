3a494c281a24c4ed7f018763fd04e82b77cda835
Regi Shehi <shehi.regi@gmail.com>
2020-09-05 17:41:37 +0200

    Add authentication

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+
+const User = require('../../models/User');
 
 // @route  GET API/auth
 // @desc   Test route
 // @access Public
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send('Server error');
+  }
+});
 
 module.exports = router;
