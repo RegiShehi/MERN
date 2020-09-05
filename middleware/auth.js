3c03add97b03bb0e0825fa87f45749e5a19691bb
Regi Shehi <shehi.regi@gmail.com>
2020-09-05 17:45:48 +0200

    Switch to token bearer

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,18 +2,34 @@ const jwt = require('jsonwebtoken');
 const config = require('config');
 
 module.exports = function (req, res, next) {
-  const token = req.header('x-auth-token');
+  // const token = req.header('x-auth-token');
+  const authHeader = req.headers.authorization;
 
-  if (!token) {
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
+
+    try {
+      const decoded = jwt.verify(token, config.get('jwtSecret'));
+
+      req.user = decoded.user;
+      next();
+    } catch (error) {
+      res.status(401).json({ msg: 'Token is not valid' });
+    }
+  } else {
     return res.status(401).json({ msg: 'No token, authorization denied' });
   }
 
-  try {
-    const decoded = jwt.verify(token, config.get('jwtSecret'));
+  // if (!token) {
+  //   return res.status(401).json({ msg: 'No token, authorization denied' });
+  // }
 
-    req.user = decoded.user;
-    next();
-  } catch (error) {
-    res.status(401).json({ msg: 'Token is not valid' });
-  }
+  // try {
+  //   const decoded = jwt.verify(token, config.get('jwtSecret'));
+
+  //   req.user = decoded.user;
+  //   next();
+  // } catch (error) {
+  //   res.status(401).json({ msg: 'Token is not valid' });
+  // }
 };
